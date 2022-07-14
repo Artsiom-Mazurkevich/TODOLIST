3d7d8450ea662b395c55e2a61a60dd9f1cfd605f
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-07-14 23:13:48 +0300

    Lesson 17 / todolists-reducer / slice

--- src/FEATURES/TodolistsList/todolists-reducer.ts
+++ src/FEATURES/TodolistsList/todolists-reducer.ts
@@ -11,16 +11,28 @@ export const slice = createSlice({
     initialState: initialState,
     reducers: {
         removeTodolistAC: (state, action: PayloadAction<{ id: string }>) => {
+            const index = state.findIndex(tl => tl.id === action.payload.id)
+            if (index > -1) {
+                state.splice(index, 1)
+            }
         },
         addTodolistAC: (state, action: PayloadAction<{ todolist: TodolistType }>) => {
+            state.push({...action.payload.todolist, filter: 'all', entityStatus: 'idle'})
         },
         changeTodolistTitleAC: (state, action: PayloadAction<{ id: string, title: string }>) => {
+            const index = state.findIndex(tl => tl.id === action.payload.id)
+            state[index].title = action.payload.title
         },
         changeTodolistFilterAC: (state, action: PayloadAction<{ id: string, filter: FilterValuesType }>) => {
+            const index = state.findIndex(tl => tl.id === action.payload.id)
+            state[index].filter = action.payload.filter
         },
         changeTodolistEntityStatusAC: (state, action: PayloadAction<{ id: string, status: RequestStatusType }>) => {
+            const index = state.findIndex(tl => tl.id === action.payload.id)
+            state[index].entityStatus = action.payload.status
         },
         setTodolistsAC: (state, action: PayloadAction<{ todolists: Array<TodolistType> }>) => {
+            return action.payload.todolists.map(tl => ({...tl, filter: 'all', entityStatus: 'idle'}))
         },
     }
 });
